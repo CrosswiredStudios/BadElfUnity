1a2f88ffe9c014790b367fb77bbcaf26c68e722e
Matthew Wood <c-matthew.wood@charter.com>
2024-05-24 10:57:41 -0700

    initial commit

---
+++ Runtime/Plugins/iOS/BadElfSdk.xcframework/ios-arm64_x86_64-maccatalyst/BadElfSdk.framework/Headers/BadElfSdk.h
@@ -0,0 +1,18 @@
+//
+//  BadElfSdk.h
+//  BadElfSdk
+//
+//  Created by Matthew Wood on 5/24/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BadElfSdk.
+FOUNDATION_EXPORT double BadElfSdkVersionNumber;
+
+//! Project version string for BadElfSdk.
+FOUNDATION_EXPORT const unsigned char BadElfSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BadElfSdk/PublicHeader.h>
+
+
